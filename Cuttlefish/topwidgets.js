faa9c87cadfb2ebedee766e9c6d13a389d8d943e
NO.ONE <42521386+Jagerx@users.noreply.github.com>
2022-11-30 23:41:08 +0300

    Update topwidgets.js

--- Cuttlefish/topwidgets.js
+++ Cuttlefish/topwidgets.js
@@ -17,7 +17,7 @@
 [rewrite_local]
 
 # ～ TopWidgets☆永久VIP（2022-10-30）@ddgksf2013
-^https?:\/\/.*xiaozujian\.com\/api\/app\/config\/userConfig url script-response-body https://codeberg.org/ddgksf2013/Cuttlefish/raw/branch/master/Crack/topwidgets.js
+^https?:\/\/.*xiaozujian\.com\/api\/app\/config\/userConfig url script-response-body https://raw.githubusercontent.com/Jagerx/QuantumultFOX/main/Cuttlefish/topwidgets.js
 
 [mitm]
 
