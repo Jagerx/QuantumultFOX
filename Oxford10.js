b9e306bfaf279eb759566424a640f8617b2adc92
NO.ONE <42521386+Jagerx@users.noreply.github.com>
2024-10-24 09:49:16 +0800

    Update Oxford10.js

--- Oxford10.js
+++ Oxford10.js
@@ -1,5 +1,3 @@
-/*
-
 项目名称：牛津高阶词典第十版
 软件版本：1.0.7
 下载地址：https://is.gd/FfzLnu
@@ -7,7 +5,7 @@
 
 
 [rewrite_local]
-^https:\/\/oxfordx\.cp\.com\.cn\/api\/pay\/apple_notify url script-response-body https://raw.githubusercontent.com/Jagerx/QuantumultFOX/main/Oxford10.js
+^https:\/\/oxfordx\.cp\.com\.cn\/api\/pay\/apple_notify url script-response-body https://raw.githubusercontent.com/yaheex/Qx/main/chxm1023/niujin10.js
 #去除首页下方广告
 ^https:\/\/oxadmin\.cp\.com\.cn\/api\/hot\/index url reject-dict
 #去除首页下方广告
@@ -17,9 +15,6 @@
 
 [mitm]
 hostname = oxfordx.cp.com.cn
-
-*/
-
 
 
 var body = JSON.parse($response.body);
