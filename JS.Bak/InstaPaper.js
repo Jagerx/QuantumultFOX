99db389cf4f0fab9045f47fd5fb6d5130f4c2e3b
NO.ONE <42521386+Jagerx@users.noreply.github.com>
2025-09-15 16:48:35 +0800

    Delete JS.Bak/InstaPaper.js

--- JS.Bak/InstaPaper.js
+++
@@ -1,5 +0,0 @@
-[rewrite_local]
-^https:\/\/www\.instapaper\.com\/api\/subscription_status url script-response-body https://raw.githubusercontent.com/saymiss/QxJs/master/Scripts/Instapaper.js
-
-[mitm]
-hostname = www.instapaper.com
